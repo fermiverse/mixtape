79466014e617af02d5a3322e374bd57707d0b3cd
fermi <fermiverse@gmail.com>
2020-10-28 02:38:50 +0530

    Design basic home pane

--- src/App.js
+++ src/App.js
@@ -1,10 +1,10 @@
-import logo from './logo.svg';
-import './App.css';
+import React from 'react';
+import Pane from './components/Pane';
 
 function App() {
   return (
     <div className="App">
-      
+      <Pane />
     </div>
   );
 }
---
+++ src/components/Pane.js
@@ -0,0 +1,35 @@
+import React from 'react';
+import axios from 'axios';
+import casetteImage from '../graphics/casette.svg';
+
+
+const authorize = async () => {
+    await axios.get(`https://accounts.spotify.com/authorize`, {
+        params: {
+            client_id: "8ce69e43f7bc4fc68b3197dc60f1a821",
+            response_type: "token",
+            redirect_uri: "http://localhost:3000/"
+        }
+    }).then(res => {
+        console.log(res.data);
+    }).catch(err => {
+        console.log("Error reaching spotify", err);
+    });
+}
+const Pane = () => {
+
+    return ( 
+        <div id="pane">
+            <div style={{position: "absolute", top: "30px"}}>
+                <h1 id="main">mixtape</h1>
+                <div id="msg">Hi PT, this is a little playlist I made to keep you company on your journey home</div>
+            </div>
+            <img id="mix" src={casetteImage} alt="start" title="mixtape" width="100%"></img>
+            <button id="spot" onClick={(e) => {
+                authorize();
+            }}>Connect Spotify</button>
+        </div>
+    );
+}
+ 
+export default Pane;
