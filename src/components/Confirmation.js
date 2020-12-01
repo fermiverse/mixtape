ee4547f0b9bb462111f5d43cf6e60e8bf4c69f97
fermi <fermiverse@gmail.com>
2020-12-01 17:53:01 +0530

    Minor changes to mix add flow

--- src/components/Confirmation.js
+++ src/components/Confirmation.js
@@ -1,11 +1,12 @@
 import React from 'react';
 import closeIcon from '../graphics/cancel.svg';
 
-const Confirmation = ({showConfirmation, toggleShowConfirmation, message}) => {
+const Confirmation = ({showConfirmation, toggleShowConfirmation, message, type}) => {
     return ( 
         <div id="modal">
             <img src={closeIcon} className="close" alt="close" title="Close" width="20px" height="20px" onClick={() => {
-                toggleShowConfirmation(false);
+                if (type === "add") window.location.reload();
+                else toggleShowConfirmation(false);
             }}></img>
             <p id={`pdes`} style={{color: "rgb(77, 74, 74)"}}>{message.slice(0, 180)}</p>
         </div>
